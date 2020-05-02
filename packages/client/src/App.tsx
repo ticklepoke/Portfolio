2515157036d792175e6948b919750d4726e544ea
Nigel <nigel@resynctech.com>
2020-05-02 20:39:09 +0800

    style(linting): added eslint, prettier and husky

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
+import React from 'react'
 // import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-   Production site
-    </div>
-  );
+function App () {
+  return <div className="App">Production site</div>
 }
 
-export default App;
+export default App
