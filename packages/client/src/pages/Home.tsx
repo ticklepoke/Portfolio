0dd345a7131e93f80ab887ba6f4b8e6840838dba
Nigel <nigel@resynctech.com>
2020-05-03 11:44:24 +0800

    feat(Added head tag): Head tag

--- packages/client/src/pages/Home.tsx
+++ packages/client/src/pages/Home.tsx
@@ -4,7 +4,10 @@ import HelmetWrapper from '../common/HelmetWrapper'
 export default function Home () {
   return (
     <div>
-      <HelmetWrapper title="Home | Nigel Lee" description="Home page" />
+      <HelmetWrapper
+        title="Home | Nigel Lee"
+        description="Computer Science student from Singapore, actively looking for software internships. Renaissance Engineering Program Student."
+      />
     </div>
   )
 }
