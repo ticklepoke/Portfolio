eccb571fcaf07f89313b9ad2f422fc0e65e9f34c
Nigel <nigel@resynctech.com>
2020-05-03 10:39:58 +0800

    feat(router): Added router, pages, lazy loading

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
-// import logo from './logo.svg';
-import './App.css'
+import React, { Suspense, lazy } from 'react'
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
+import ErrorBoundary from './common/ErrorBoundary'
+
+// import './App.css'
+
+const HomeLazy = lazy(() => import('./pages/Home'))
+
+const ExperienceLazy = lazy(() => import('./pages/Experience'))
 
 function App () {
-  return <div className="App">Hello world</div>
+  return (
+    <ErrorBoundary>
+      <Suspense fallback={<span>Loading...</span>}>
+        <Router>
+          <h1>Header</h1>
+          <Switch>
+            <Route path="/" exact>
+              <HomeLazy />
+            </Route>
+            <Route path="/experience" exact>
+              <ExperienceLazy />
+            </Route>
+          </Switch>
+        </Router>
+      </Suspense>
+    </ErrorBoundary>
+  )
 }
 
 export default App
