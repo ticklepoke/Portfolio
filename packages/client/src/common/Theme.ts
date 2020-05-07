c6b19fdca5bb3d5d46bc2d8c9df570d0d275e6e6
Nigel <nigel@resynctech.com>
2020-05-07 11:05:40 +0800

    feat(Experience): Added experience page

--- packages/client/src/common/Theme.ts
+++ packages/client/src/common/Theme.ts
@@ -1,9 +1,11 @@
 export const lightTheme = {
   body: '#fff',
-  text: '#1a1a1a'
+  text: '#1a1a1a',
+  bodySecondary: '#343a40'
 }
 
 export const darkTheme = {
   body: '#343a40',
-  text: '#fff'
+  text: '#fff',
+  bodySecondary: '#fff'
 }
--- packages/client/src/pages/Experience.tsx
+++ packages/client/src/pages/Experience.tsx
@@ -1,6 +1,22 @@
 import React, { Fragment } from 'react'
 import HelmetWrapper from '../common/HelmetWrapper'
-
+import { experiences } from '../_data/Experience'
+const renderCards = () => {
+  const cards = experiences.map(experience => {
+    return (
+      <div key={experience.title} className="mb-3 col-md-4">
+        <div className="card mx-2" style={{ minHeight: '100%' }}>
+          <div className="card-header">{experience.date}</div>
+          <div className="card-body">
+            <h4 className="card-title">{experience.title}</h4>
+            <p className="card-text">{experience.description}</p>
+          </div>
+        </div>
+      </div>
+    )
+  })
+  return cards
+}
 export default function Experience () {
   return (
     <Fragment>
@@ -8,6 +24,13 @@ export default function Experience () {
         title="Experiences | Nigel Lee"
         description="SG Innovate Summation Program Apprentice, DBS Unicorn+ Tech Intern, Renaissance Engineering Program Student"
       />
+      <div className="container my-5">
+        <h1>Experience</h1>
+        <h3 className="my-5">
+          I&lsquo;ve worked at a few places and am eager for more opportunities.
+        </h3>
+        <div className="row no-gutters">{renderCards()}</div>
+      </div>
     </Fragment>
   )
 }
